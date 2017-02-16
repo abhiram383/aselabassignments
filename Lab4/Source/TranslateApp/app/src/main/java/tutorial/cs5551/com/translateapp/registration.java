06ff965cb4192c61507b51639b306ac2b37694d7
abhiram383 <ramleela@383>
2017-02-15 18:55:04 -0600

    lab4

---
+++ Lab4/Source/TranslateApp/app/src/main/java/tutorial/cs5551/com/translateapp/registration.java
@@ -0,0 +1,15 @@
+package tutorial.cs5551.com.translateapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+
+}
